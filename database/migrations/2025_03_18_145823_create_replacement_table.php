d7c71965138b54ba40f6e5fd211426cd325c2b81
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2025-03-18 16:04:46 +0100

    feat: Add DB migrations :trollface:

---
+++ database/migrations/2025_03_18_145823_create_replacement_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('replacement', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->integer('amount');
+            $table->double('price');
+            $table->string('state');
+            $table->foreignId('brand_id')->constrained('brand')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('replacement');
+    }
+};
