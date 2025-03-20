5658a6e0f13a7153e5b8e9fe5099b90f0cad0b15
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-20 13:01:11 +0100

    fix: names APIs

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ReplacementController;
 use App\Http\Controllers\Api\ApplianceController;
 
-Route::get("/replacement", [ReplacementController::class, "index"])->name("index");
+Route::get("/replacement", [ReplacementController::class, "index"])->name("apiireplacement");
 Route::get("/replacement/{id}", [ReplacementController::class, "show"])->name("show");
 Route::post("/replacement", [ReplacementController::class, "store"])->name("store");
 Route::put("/replacement/{id}", [ReplacementController::class, "update"])->name("update");
 Route::delete("/replacement/{id}", [ReplacementController::class, "destroy"])->name("destroy");
 
-Route::get("/liance", [ApplianceController::class, "index"])->name("apiiFlights");
+Route::get("/liance", [ApplianceController::class, "index"])->name("apiiliance");
 Route::get("/liance/{id}", [ApplianceController::class, "show"])->name("apiishow");
 Route::post("/liance", [ApplianceController::class, "store"])->name("apiistore");
 Route::put("/liance/{id}", [ApplianceController::class, "update"])->name("apiiupdate");
