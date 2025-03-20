ede2795e2ebdbadaeeef3e5c6843f21b3ede9d0f
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-20 13:22:37 +0100

    Feat: Factories && seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Replacement;
+use App\Models\Appliance;
+use App\Models\Brand;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +17,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+            
+        ]);*/
     }
 }
+
+Appliance::factory(10)->create();
+
+Brand::factory(10)->create();
+
+Replacement::factory(10)->create();
