ef49160ef78be37f3a22e2c7b9d46dd10e0bb4b0
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-20 10:02:43 +0100

    Refactor: Appliance

--- app/Models/Appliance.php
+++ app/Models/Appliance.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Appliance extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'model',
+        'brand_id',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
