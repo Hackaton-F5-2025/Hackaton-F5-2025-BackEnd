bd4d1a8f29d1553b4e32bb87a988da468c2a991e
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-20 12:57:35 +0100

    feat: Api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ReplacementController;
+use App\Http\Controllers\Api\ApplianceController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get("/replacement", [ReplacementController::class, "index"])->name("index");
+Route::get("/replacement/{id}", [ReplacementController::class, "show"])->name("show");
+Route::post("/replacement", [ReplacementController::class, "store"])->name("store");
+Route::put("/replacement/{id}", [ReplacementController::class, "update"])->name("update");
+Route::delete("/replacement/{id}", [ReplacementController::class, "destroy"])->name("destroy");
+
+Route::get("/liance", [ApplianceController::class, "index"])->name("apiiFlights");
+Route::get("/liance/{id}", [ApplianceController::class, "show"])->name("apiishow");
+Route::post("/liance", [ApplianceController::class, "store"])->name("apiistore");
+Route::put("/liance/{id}", [ApplianceController::class, "update"])->name("apiiupdate");
+Route::delete("/liance/{id}", [ApplianceController::class, "destroy"])->name("apiidestroy");
