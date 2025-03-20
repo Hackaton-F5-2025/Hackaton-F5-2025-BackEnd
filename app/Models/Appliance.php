d18ea2c8369928869f3bf35e361908bbce034261
Leonkeneddy86 <jonathan19.jtv@gmail.com>
2025-03-20 10:05:30 +0100

    fix: Appliance relations

--- app/Models/Appliance.php
+++ app/Models/Appliance.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Appliance extends Model
 {
     use HasFactory;
